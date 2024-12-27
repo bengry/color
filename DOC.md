03a85bbd88862338aa819c23f2c549e70b02bd70
mattdesl <info@mattdesl.studio>
2024-12-27 10:34:21 +0000

    adding docs

---
+++ DOC.md
@@ -0,0 +1,17 @@
+`@texel/color` is a minimal and modern color library for JavaScript. Especially useful for real-time applications, generative art, and graphics on the web.
+
+## Source Code
+
+The source code is on GitHub:
+
+[https://github.com/texel-org/color](https://github.com/texel-org/color)
+
+## Docs
+
+The documentation is held here:
+
+[https://texel-org.github.io/color](https://texel-org.github.io/color)
+
+## Usage
+
+For installation and usage, see the [README.md page](https://github.com/texel-org/color/) on the GitHub repository.
--- src/spaces.js
+++ src/spaces.js
@@ -1,3 +1,9 @@
+/**
+ * List of color spaces and their corresponding modules.
+ *
+ * @module spaces
+ */
+
 import { XYZ, XYZD50 } from "./spaces/xyz.js";
 import { OKLab, OKLCH, OKHSV, OKHSL } from "./spaces/oklab.js";
 import { sRGB, sRGBLinear, sRGBGamut } from "./spaces/srgb.js";
@@ -18,6 +24,23 @@ export * from "./spaces/rec2020.js";
 export * from "./spaces/a98-rgb.js";
 export * from "./spaces/prophoto-rgb.js";
 
+/**
+ * Exports all color space modules.
+ * @see module:spaces/xyz
+ * @see module:spaces/oklab
+ * @see module:spaces/srgb
+ * @see module:spaces/display-p3
+ * @see module:spaces/rec2020
+ * @see module:spaces/a98-rgb
+ * @see module:spaces/prophoto-rgb
+ */
+
+/**
+ * Returns a list of color spaces.
+ *
+ * @function
+ * @returns {Array} An array of color space objects.
+ */
 export const listColorSpaces = () => {
   return [
     XYZ, // D65
@@ -39,6 +62,12 @@ export const listColorSpaces = () => {
   ];
 };
 
+/**
+ * Returns a list of color gamuts.
+ *
+ * @function
+ * @returns {Array} An array of color gamut objects.
+ */
 export const listColorGamuts = () => {
   return [sRGBGamut, DisplayP3Gamut, Rec2020Gamut, A98RGBGamut];
 };
