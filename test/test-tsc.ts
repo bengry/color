b4833c69906d1d02ccb19765822e8dd4bd2c6169
mattdesl <info@mattdesl.studio>
2024-12-27 16:22:54 +0000

    type fixes

--- test/test-tsc.ts
+++ test/test-tsc.ts
@@ -1,4 +1,4 @@
-///<reference path="../types/types.d.ts" />
+///<reference path="../types.d.ts" />
 import { listColorSpaces, serialize, sRGB } from "../src/index.js";
 console.log(serialize([0.5, 0.25, 0.15], sRGB), listColorSpaces()[0].id);
 
